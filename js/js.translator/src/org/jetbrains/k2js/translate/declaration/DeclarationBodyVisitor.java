1b9897ec7fa1a044eb331bbf2092dfa0d411107f
Erokhin Stanislav <Stanislav.Erokhin@jetbrains.com>
2013-10-18 21:09:14 +0400

    JS backend: rewrite FileDeclarationVisitor use kotlin

--- js/js.translator/src/org/jetbrains/k2js/translate/declaration/DeclarationBodyVisitor.java
+++ js/js.translator/src/org/jetbrains/k2js/translate/declaration/DeclarationBodyVisitor.java
@@ -20,6 +20,7 @@ import com.google.dart.compiler.backend.js.ast.JsExpression;
 import com.google.dart.compiler.backend.js.ast.JsFunction;
 import com.google.dart.compiler.backend.js.ast.JsPropertyInitializer;
 import com.intellij.util.SmartList;
+import jet.runtime.typeinfo.KotlinSignature;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jet.lang.descriptors.ClassDescriptor;
 import org.jetbrains.jet.lang.descriptors.FunctionDescriptor;
@@ -42,7 +43,7 @@ import static org.jetbrains.k2js.translate.utils.BindingUtils.getClassDescriptor
 import static org.jetbrains.k2js.translate.utils.BindingUtils.getFunctionDescriptor;
 
 public class DeclarationBodyVisitor extends TranslatorVisitor<Void> {
-    protected final List<JsPropertyInitializer> result;
+    @KotlinSignature("val result: MutableList<JsPropertyInitializer>") protected final List<JsPropertyInitializer> result;
     protected final List<JsPropertyInitializer> staticResult;
     protected final List<JsPropertyInitializer> enumEntryList = new SmartList<JsPropertyInitializer>();
 
@@ -65,7 +66,7 @@ public class DeclarationBodyVisitor extends TranslatorVisitor<Void> {
     }
 
     @Override
-    public Void visitClass(@NotNull JetClass expression, @NotNull TranslationContext context) {
+    public Void visitClass(@NotNull JetClass expression, TranslationContext context) {
         return null;
     }
 
@@ -85,9 +86,7 @@ public class DeclarationBodyVisitor extends TranslatorVisitor<Void> {
     }
 
     @Override
-    public Void visitClassObject(
-            @NotNull JetClassObject classObject, TranslationContext context
-    ) {
+    public Void visitClassObject(@NotNull JetClassObject classObject, TranslationContext context) {
         JetObjectDeclaration declaration = classObject.getObjectDeclaration();
         assert declaration != null : "Declaration for class object must be not null";
         ClassDescriptor descriptor = getClassDescriptor(context.bindingContext(), declaration);
@@ -99,13 +98,13 @@ public class DeclarationBodyVisitor extends TranslatorVisitor<Void> {
     }
 
     @Override
-    public Void visitObjectDeclaration(@NotNull JetObjectDeclaration declaration, @NotNull TranslationContext context) {
+    public Void visitObjectDeclaration(@NotNull JetObjectDeclaration declaration, TranslationContext context) {
         // parsed it in initializer visitor => no additional actions are needed
         return null;
     }
 
     @Override
-    public Void visitNamedFunction(@NotNull JetNamedFunction expression, @NotNull TranslationContext context) {
+    public Void visitNamedFunction(@NotNull JetNamedFunction expression, TranslationContext context) {
         FunctionDescriptor descriptor = getFunctionDescriptor(context.bindingContext(), expression);
         if (descriptor.getModality() == Modality.ABSTRACT) {
             return null;
@@ -117,14 +116,14 @@ public class DeclarationBodyVisitor extends TranslatorVisitor<Void> {
     }
 
     @Override
-    public Void visitProperty(@NotNull JetProperty expression, @NotNull TranslationContext context) {
+    public Void visitProperty(@NotNull JetProperty expression, TranslationContext context) {
         PropertyDescriptor propertyDescriptor = BindingUtils.getPropertyDescriptor(context.bindingContext(), expression);
         PropertyTranslator.translateAccessors(propertyDescriptor, expression, result, context);
         return null;
     }
 
     @Override
-    public Void visitAnonymousInitializer(@NotNull JetClassInitializer expression, @NotNull TranslationContext context) {
+    public Void visitAnonymousInitializer(@NotNull JetClassInitializer expression, TranslationContext context) {
         // parsed it in initializer visitor => no additional actions are needed
         return null;
     }
